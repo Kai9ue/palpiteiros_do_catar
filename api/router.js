5e7ede51894c10e13df2e84d44d19089e06f48d8
Kai9ue <kaiquecarvalholive@hotmail.com>
2022-10-08 08:57:00 -0300

    list games

--- api/router.js
+++ api/router.js
@@ -1,6 +1,7 @@
 import Router from '@koa/router'
 import * as users from './app/users/index.js'
 import * as hunches from './app/hunches/index.js'
+import * as games from './app/games/index.js'
 
 export const router = new Router()
 
@@ -9,3 +10,5 @@ router.post('/users', users.create)
 
 //router.get('/hunches', hunches.list)
 router.post('/hunches', hunches.create)
+
+router.get('/games', games.list)
